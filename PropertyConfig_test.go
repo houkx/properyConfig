dbe30d4f1ee64f95efd9e4c0f69a0a9d6e03a195
aidream <1084940623@qq.com>
2021-02-07 21:43:46 +0800

    Update PropertyConfig_test.go

--- PropertyConfig_test.go
+++ PropertyConfig_test.go
@@ -1,12 +1,11 @@
 package properyConfig
 
+
 import (
 	"context"
 	"fmt"
-	"github.com/magiconair/properties"
-	"log"
-	"os"
 	"testing"
+	"time"
 )
 
 type ConfTest struct {
@@ -14,12 +13,12 @@ type ConfTest struct {
 	Port   int   `properties:"server.port,default=8000"`
 }
 
-// setter OR field
 func (p *ConfTest) SetTestGo(v uint8) {
 	p.testGo = v
 }
 
 type AppConf struct {
+	notConf  bool
 	TestGo   int            `properties:"test.go,default=1"`
 	TestDef  int            `properties:"test.def,default=1024"`
 	Proxy    map[string]int `properties:",default={}"`
@@ -28,7 +27,7 @@ type AppConf struct {
 
 func (p *AppConf) Init() {
 	fmt.Println("AppConf Init() Finished! TestDef =", p.TestDef,
-		",Proxy =", p.Proxy, ",DefProxy =", p.DefProxy)
+		",Proxy =", p.Proxy, ",DefProxy =", p.DefProxy, p.notConf)
 }
 
 func TestConfig_api(t *testing.T) {
@@ -41,25 +40,6 @@ func TestConfig_api(t *testing.T) {
 	ctx, _ := context.WithCancel(context.Background())
 	//Loop: CheckUpdate
 	ps.ScheduleCheckUpdate(ctx)
+	time.Sleep(time.Second * 65)
 	ps.Close()
-}
-func TestConfig_internal(t *testing.T) {
-	obj := &ConfTest{}
-	vSet := make(map[string][]ValueSetter, 0)
-	logger := log.New(os.Stdout, "INFO ", log.Lshortfile|log.Ldate|log.Ltime)
-	var appConf = &AppConf{}
-	initValueSetter(logger, obj, vSet)
-	initValueSetter(logger, appConf, vSet)
-	ps := properties.MustLoadString(`
-          Proxy./el=9070
-          Proxy./el/api/usertag=8083
-          # test
-          server.port=9081
-          test.go=19
-    `)
-	setConfigValues(ps.Map(), vSet)
-	t.Log("testGo =", obj.testGo)
-	t.Log("Port =", obj.Port)
-	t.Log("appConf.TestGo =", appConf.TestGo)
-	t.Log("appConf.Proxy =", appConf.Proxy)
 }
